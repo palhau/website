487efa079b31ba24afa28069946d774b6040f83c
Alan Palhau <alanvieirapalhau@hotmail.com>
2023-06-01 07:06:46 -0300

    hotfix: interface declaration on types utils

--- utils/types/index.ts
+++ utils/types/index.ts
@@ -3,9 +3,3 @@ export type isPathMatchType = {
   projects: boolean;
   work: boolean;
 };
-
-export interface EventTarget {
-  addEventListener(type: string, listener: EventListenerOrEventListenerObject, useCapture?: boolean): void;
-  dispatchEvent(evt: Event): boolean;
-  removeEventListener(type: string, listener: EventListenerOrEventListenerObject, useCapture?: boolean): void;
-}
