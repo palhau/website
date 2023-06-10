6cc6ea717466c2a3ffd7d7867ef52dc4a0676b77
Alan Palhau <alanvieirapalhau@hotmail.com>
2023-06-10 08:22:03 -0300

    hotfix: spelling and profile picture

--- components/Home/ProfessionalResume/index.tsx
+++ components/Home/ProfessionalResume/index.tsx
@@ -5,12 +5,12 @@ import Link from 'next/link';
 export const ProfessionalResume = () => {
   return (
     <div className={styles.container}>
-      <h3>Work</h3>
+      <h3>About</h3>
       <p>
-        Alan Palhau is a freelance, frontend, and full-stack developer based in
+        Alan Palhau is a freelance, frontend developer based in
         Brazil, with a love in solve puzzles and building/creating apps and
-        websites to help solve these &quot;puzzles&quot;, that could be referred
-        to as real-life problems converted to code that programmers and machines
+        websites to help solve these &quot;issues&quot;, that could be referred
+        to as real-life problems converted to code that programmers and machines can easily
         understand.
       </p>
       {/* <div id="Button" className={styles.projectsBtn}>
